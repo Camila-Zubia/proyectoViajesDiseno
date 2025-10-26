078d72f2aab4a676b88bb6745d4eb267dbcbcfc1
Camila-Zubia <cami.zubia@gmail.com>
2025-10-25 21:56:44 -0700

    proyecto DTO

---
+++ dto_negocios/src/main/java/dto/ParadaDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+/**
+ *
+ * @author Camila Zubia 00000244825
+ */
+public class ParadaDTO {
+    
+}
