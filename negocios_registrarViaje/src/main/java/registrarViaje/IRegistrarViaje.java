cf3dbf991b5131bad9d74c6a6ec3db055d28580d
Camila-Zubia <cami.zubia@gmail.com>
2025-10-25 23:34:58 -0700

    interfaz e implementacion del subsistema
    falta mockeo y pruebas unitarias

---
+++ negocios_registrarViaje/src/main/java/registrarViaje/IRegistrarViaje.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package registrarViaje;
+
+import dto.ParadaDTO;
+import dto.VehiculoDTO;
+import dto.ViajeDTO;
+import java.util.List;
+
+/**
+ *interfaz que define las operaciones del caso de uso Registrar Viaje
+ * @author Camila Zubia 00000244825
+ */
+public interface IRegistrarViaje {
+    
+    /**
+     * crea un nuevo viaje en el sistema
+     * @param viaje objeto que contiene la info como origen, destino, fecha, etc.
+     */
+    public void crearViaje(ViajeDTO viaje);
+    
+    
+    /**
+     * permite obtener el objeto vehiculo que fue seleccionado, para agregarlo
+     * al viaje
+     * @param vehiculo contiene la marca, color capacidad, etc.
+     * @return regresa el vehiculo seleccionado
+     */
+    public VehiculoDTO obtenerVehiculo(VehiculoDTO vehiculo);
+    
+    /**
+     * recibe la lisra de vehiculos que el conductor tiene registrados
+     * @return la lista de vehiculos
+     */
+    public List<VehiculoDTO> obtenerVehiculos();
+    
+    /**
+     * valida que el viaje que se esta registrando, no exista en el sistema
+     * @return un bollean true = existe, false = no existe
+     */
+    public boolean validarNoExiste();
+    
+    /**
+     * agrega las paradas a la lista del viaje que se esta registrando
+     * @param paradas lista
+     */
+    public void agregarAListaParadas(List<ParadaDTO> paradas);
+    
+    /**
+     * crea una nueva parada y la asocia al viaje actual
+     * @param parada objeto que tiene la direccion y el precio
+     */
+    public void crearParada(ParadaDTO parada);
+}
---
+++ negocios_registrarViaje/src/main/java/registrarViaje/RegistrarViaje.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package registrarViaje;
+
+import dto.ParadaDTO;
+import dto.VehiculoDTO;
+import dto.ViajeDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Camila Zubia 00000244825
+ */
+public class RegistrarViaje implements IRegistrarViaje{
+
+    @Override
+    public void crearViaje(ViajeDTO viaje) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public VehiculoDTO obtenerVehiculo(VehiculoDTO vehiculo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<VehiculoDTO> obtenerVehiculos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean validarNoExiste() {
+        return false;
+    }
+
+    @Override
+    public void agregarAListaParadas(List<ParadaDTO> paradas) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void crearParada(ParadaDTO parada) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
