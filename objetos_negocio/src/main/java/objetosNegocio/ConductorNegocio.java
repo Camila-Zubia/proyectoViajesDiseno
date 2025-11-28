edc8b52753c9a798cba9999e029d5321f824b293
Adel Mendez <adelmendez2@gmail.com>
2025-11-27 20:09:01 -0700

    Revert "adaptador"
    This reverts commit dd256b3c7a5a8ed0f3e8a7c8d62223cbf7059c45.

--- objetos_negocio/src/main/java/objetosNegocio/ConductorNegocio.java
+++ objetos_negocio/src/main/java/objetosNegocio/ConductorNegocio.java
@@ -4,46 +4,39 @@
  */
 package objetosNegocio;
 
-import adaptadores.adaptadorVehiculo;
 import dto.VehiculoDTO;
 import dto.ViajeDTO;
 import interfaces.IConductorNegocio;
 import java.util.List;
-import org.base_datos_viajes.dao.impl.ConductorDAO;
 import utilidades.SesionUsuario;
-import org.base_datos_viajes.dao.interfaces.IConductorDAO;
 
 /**
  *
  * @author Camila Zubia 00000244825
  */
-public class ConductorNegocio implements IConductorNegocio {
-
-    IConductorDAO conductoDAO = new ConductorDAO();
-    adaptadorVehiculo adaptadorV = new adaptadorVehiculo();
-
+public class ConductorNegocio implements IConductorNegocio{
+    
     public ConductorNegocio() {
-
+        
     }
-
+    
     @Override
-    public void agregarVehiculo(VehiculoDTO vehiculo) {
+    public void agregarVehiculo(VehiculoDTO vehiculo){
         SesionUsuario.obtenerConductor().getVehiculos().add(vehiculo);
-
     }
-
+    
     @Override
-    public List<VehiculoDTO> obtenerVehiculos() {
-        return adaptadorV.ListaVehiculoToDTO(conductoDAO.obtenerVehiculos(SesionUsuario.obtenerConductor().getId()));
+    public List<VehiculoDTO> obtenerVehiculos(){
+        return SesionUsuario.obtenerConductor().getVehiculos();
     }
-
+    
     @Override
-    public void agregarViaje(ViajeDTO viaje) {
+    public void agregarViaje(ViajeDTO viaje){
         SesionUsuario.obtenerConductor().getViajes().add(viaje);
     }
-
+    
     @Override
-    public List<ViajeDTO> obtenerViajes() {
+    public List<ViajeDTO> obtenerViajes(){
         return SesionUsuario.obtenerConductor().getViajes();
     }
 }
