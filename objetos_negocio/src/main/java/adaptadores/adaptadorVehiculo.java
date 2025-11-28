dd256b3c7a5a8ed0f3e8a7c8d62223cbf7059c45
Adel Mendez <adelmendez2@gmail.com>
2025-11-27 19:20:40 -0700

    adaptador

--- objetos_negocio/src/main/java/adaptadores/adaptadorVehiculo.java
+++ objetos_negocio/src/main/java/adaptadores/adaptadorVehiculo.java
@@ -1,7 +1,8 @@
-
 package adaptadores;
 
 import dto.VehiculoDTO;
+import java.util.ArrayList;
+import java.util.List;
 import org.base_datos_viajes.model.Vehiculo;
 import org.bson.types.ObjectId;
 
@@ -10,13 +11,14 @@ import org.bson.types.ObjectId;
  * @author ferch
  */
 public class adaptadorVehiculo {
+
     public static VehiculoDTO toDTO(Vehiculo entidad) {
         VehiculoDTO dto = new VehiculoDTO();
 
-        if (entidad.getId() != null) { 
-             dto.setId(entidad.getId().toString()); 
+        if (entidad.getId() != null) {
+            dto.setId(entidad.getId().toString());
         }
-        
+
         dto.setModelo(entidad.getModelo());
         dto.setPlacas(entidad.getPlacas());
         dto.setMarca(entidad.getMarca());
@@ -27,17 +29,49 @@ public class adaptadorVehiculo {
 
     public static Vehiculo toEntity(VehiculoDTO dto) {
         Vehiculo entidad = new Vehiculo(
-            dto.getMarca(),
-            dto.getModelo(),
-            dto.getColor(),
-            null, 
-            dto.getPlacas(),
-            dto.getCapacidad()
+                dto.getMarca(),
+                dto.getModelo(),
+                dto.getColor(),
+                null,
+                dto.getPlacas(),
+                dto.getCapacidad()
         );
 
         if (dto.getId() != null) {
-            entidad.setId(new ObjectId(dto.getId())); 
+            entidad.setId(new ObjectId(dto.getId()));
         }
         return entidad;
+    }
+
+    public List<VehiculoDTO> ListaVehiculoToDTO(List<Vehiculo> vehiculo) {
+        List<VehiculoDTO> vehiculosDTO = new ArrayList();
+        for (Vehiculo v : vehiculo) {
+            VehiculoDTO dto = new VehiculoDTO();
+            dto.setCapacidad(v.getCapacidad());
+            dto.setColor(v.getColor());
+            dto.setMarca(v.getMarca());
+            dto.setModelo(v.getModelo());
+            dto.setPlacas(v.getPlacas());
+            vehiculosDTO.add(dto);
+        }
+
+        return vehiculosDTO;
+    }
+
+    public List<Vehiculo> ListaVehiculoToEntity(List<VehiculoDTO> vehiculo) {
+        List<Vehiculo> vehiculosEnt = new ArrayList();
+        for (VehiculoDTO v : vehiculo) {
+            Vehiculo ent = new Vehiculo();
+
+            ent.setCapacidad(v.getCapacidad());
+            ent.setColor(v.getColor());
+            ent.setMarca(v.getMarca());
+            ent.setModelo(v.getModelo());
+            ent.setPlacas(v.getPlacas());
+
+            vehiculosEnt.add(ent);
+        }
+        return vehiculosEnt;
+
     }
 }
