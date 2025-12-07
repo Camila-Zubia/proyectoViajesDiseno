33259e09661608f7b514c3cf5d562358435b369c
Adel Mendez <adelmendez2@gmail.com>
2025-12-07 12:56:10 -0700

    negocio registrar vehiculo
    se añadieron los metodos jej

--- negocios_registrarVehiculo/src/main/java/registrarVehiculo/FRegistrarVehiculo.java
+++ negocios_registrarVehiculo/src/main/java/registrarVehiculo/FRegistrarVehiculo.java
@@ -8,6 +8,26 @@ package registrarVehiculo;
  *
  * @author adell
  */
-public class FRegistrarVehiculo {
-    
+public class FRegistrarVehiculo implements IRegistrarVehiculo {
+
+    private final controlRegistrarVehiculo controlVehiculo;
+
+    public FRegistrarVehiculo() {
+        controlVehiculo = new controlRegistrarVehiculo();
+    }
+
+    @Override
+    public void guardarDatosVehiculo(String modelo, String placas, String marca, String color, int capacidad) {
+        controlVehiculo.guardarDatosVehiculo(modelo, placas, marca, color, capacidad);
+    }
+
+    @Override
+    public void guardarDatosPropietario(String nombre, String curp, String rfc, String nss) {
+        controlVehiculo.guardarDatosPropietario(nombre, curp, rfc, nss);
+    }
+
+    @Override
+    public void confirmarRegistroVehiculoPropietario() {
+        controlVehiculo.confirmarRegistroVehiculoPropietario();
+    }
 }
